73f284a63687e7d7e095492811a59112a230d994
Denperidge <denperidge@gmail.com>
2023-01-31 17:33:52 +0100

    Moved dict_category_repos code into categories

--- categories.py
+++ categories.py
@@ -32,3 +32,17 @@ categories = {
     "archive": Category("Archive", "archive"),  # Ignored
     "tools": Category("Tools", "tools"),
 }
+
+def sort_into_category_dict(repos) -> dict:
+    dict_category_repos = {}
+    for category_id in categories:
+        if category_id == categories["archive"].id:
+            print("Skipping archive!")
+            continue
+        
+        category = categories[category_id]
+        category_repos = [repo for repo in repos if repo.category.id == category_id]
+
+        dict_category_repos[category.name] = category_repos
+    
+    return dict_category_repos
--- index.py
+++ index.py
@@ -1,24 +1,13 @@
 from reposource.github import GitHub
 from imagesource.dockerhub import DockerHub
-from categories import categories
+from categories import categories, sort_into_category_dict
 
 if __name__ == "__main__":
     """Get the repos, parse them, sort them by category, export them to build/*.html"""
     #repos = list_and_parse_repos()
     mu_semtech_github = GitHub(owner="mu-semtech", imagesource=DockerHub(owner="semtech"))
 
-    dict_category_repos = {}
-    for category_id in categories:
-        if category_id == categories["archive"].id:
-            print("Skipping archive!")
-            continue
-        
-        category = categories[category_id]
-        category_repos = [repo for repo in mu_semtech_github.repos if repo.category.id == category_id]
-
-        dict_category_repos[category.name] = category_repos
-    
-    print(dict_category_repos)
+    dict_category_repos = sort_into_category_dict(mu_semtech_github.repos)
 
     print(mu_semtech_github.repos[0].image)
     # api = Api.config({
