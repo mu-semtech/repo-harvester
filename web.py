7eb0183cf100a69dbeee76441496ed297ca129da
Aad Versteden <madnificent@gmail.com>
2023-02-07 10:05:07 +0100

    Turn into microservice
    We now base ourselves on the python template and create a call

--- web.py
+++ web.py
@@ -3,7 +3,8 @@ from imagesource.dockerhub import DockerHub
 from categories import categories, sort_into_category_dict
 from semtech import add_repos_to_triplestore, clear_all_triples
 
-if __name__ == "__main__":
+@app.route("/update", methods=["POST"])
+def update():
     """Get the repos, parse them, sort them by category, export them to build/*.html"""
     #repos = list_and_parse_repos()
     mu_semtech_github = GitHub(owner="mu-semtech", imagesource=DockerHub(owner="semtech"))
@@ -20,21 +21,4 @@ if __name__ == "__main__":
 
     clear_all_triples()
 
-
     add_repos_to_triplestore(mu_semtech_github.repos)
-
-    # api = Api.config({
-    #     "API_ROOT": "http://localhost/"
-    # })
-
-    # endpoint = api.endpoint('microservices')
-    
-    # for repo in mu_semtech_github.repos:
-        
-    #     endpoint.put(object=JsonApiObject(
-    #         type="microservices",
-    #         attributes = {
-    #             "title": "meowmeows"
-                
-    #         }
-    #     ))
