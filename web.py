c54d48101c500148454f1cb65ad5c289e6af7e3b
Aad Versteden <madnificent@gmail.com>
2023-02-16 12:16:37 +0100

    Use log for emitting logs in web.py

--- web.py
+++ web.py
@@ -2,8 +2,7 @@ from reposource.github import GitHub
 from imagesource.dockerhub import DockerHub
 from categories import categories, sort_into_category_dict
 from semtech import add_repos_to_triplestore, clear_all_triples
-
-
+from helpers import log
 
 @app.route("/")
 def index():
@@ -11,20 +10,20 @@ def index():
 
 @app.route("/update", methods=["GET", "POST"])
 def update():
-    print("Updating...")
+    log("Updating...")
     """Get the repos, parse them, sort them by category, export them to build/*.html"""
     #repos = list_and_parse_repos()
     mu_semtech_github = GitHub(owner="mu-semtech", imagesource=DockerHub(owner="semtech"))
 
     dict_category_repos = sort_into_category_dict(mu_semtech_github.repos)
 
-    print(dict_category_repos)
+    log(dict_category_repos)
 
     repo = mu_semtech_github.repos[8]
-    print(repo)
-    print(repo.description)
-    print(repo.image)
-    print(repo.revisions)
+    log(repo)
+    log(repo.description)
+    log(repo.image)
+    log(repo.revisions)
 
     clear_all_triples()
 
