30a699d17da2753f80a46f69d854c6a2d45d1308
Denperidge <denperidge@gmail.com>
2023-08-03 15:01:49 +0200

    Slight categories refactor, added test

--- src/app/utils/categories.py
+++ src/app/utils/categories.py
@@ -7,17 +7,24 @@ categories defines the categories to use throughout repo-harvester
 - Sort by override, then specific regex
 - Regex & category names below are based on mu-semtech naming conventions
 """
-categories = {}
 
-config = read_config("categories")
+def _load_categories_from_config(filename="categories"):
+    config = read_config(filename)
 
-for section_name in config.sections():
-    section = config[section_name]
-    category = Category(section.get("name"), section.get("id"))
-    if "regex" in section:
-        category.regex = section["regex"]
+    categories = {}
+    for section_name in config.sections():
+        section = config[section_name]
+        category = Category(section.get("name"), section.get("id"))
+        if "regex" in section:
+            category.regex = section["regex"]
+        
+        categories[section_name] = category
     
-    categories[section_name] = category
+    return categories
+
+
+categories = _load_categories_from_config()
+
 
 def sort_into_category_dict(repos: list) -> dict:
     """A function that turns a List[Repo] into a dict[category_id] = List[Repo]"""
--- test.py
+++ test.py
@@ -2,13 +2,15 @@ import unittest
 
 from src.tests.test_config_conf import TestConfig
 from src.tests.test_config_override import TestConfigOverride
+from src.tests.test_utils_categories import TestUtilsCategories
 
 
 if __name__ == "__main__":
     suites = []
     for suite in [
         TestConfig,
-        TestConfigOverride
+        TestConfigOverride,
+        TestUtilsCategories
     ]:
         suites.append(unittest.makeSuite(suite))
 
